962d44cad390ffc6ff99f11891106edd532c8eb5
Nguyễn Cao Phúc <nguyencaophuc.204@gmail.com>
2026-05-27 14:27:58 +0700

    This commit updates the `NotificationType` enum by renaming the `BOOKING_SUCCESS` value to `BOOKING_CREATED`.

--- Notification/src/main/java/com/caophuc/notification/util/NotificationType.java
+++ Notification/src/main/java/com/caophuc/notification/util/NotificationType.java
@@ -2,7 +2,8 @@ package com.caophuc.notification.util;
 
 public enum NotificationType {
     SYSTEM,          // Thông báo hệ thống
-    BOOKING_SUCCESS, // Đặt sân thành công
+    BOOKING_CREATED, // Đặt sân thành công
     BOOKING_CANCELLED,// Hủy đặt sân
     PAYMENT_FAILED   // Thanh toán thất bại
+
 }
